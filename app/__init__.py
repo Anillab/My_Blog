dcad8ee60607664ca2a612cc2c1ada9246d16b50
Anillab <anillabusolo@gmail.com>
2018-10-29 22:01:52 +0300

    initialized bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -9,5 +9,6 @@ from .main import main as main_blueprint
 def create_app(config_name):
     app=Flask(__name__)
     app.config.from_object(config_options[config_name])
+    bootstrap.init_app(app)
     app.register_blueprint(main_blueprint)
     return app
