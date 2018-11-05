0c4ab8a23b6b1633ce395d8da9b036574d59bf1e
Anillab <anillabusolo@gmail.com>
2018-11-06 01:11:12 +0300

    imported sqlalchemy initialized the db

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,16 @@ import flask
 from flask  import *
 from flask_bootstrap import Bootstrap
 from config import config_options
+from flask_sqlalchemy import SQLAlchemy
 # from main  import hello
 # from app import errors
 bootstrap=Bootstrap()
+db=SQLAlchemy()
 from .main import main as main_blueprint
 def create_app(config_name):
     app=Flask(__name__)
     app.config.from_object(config_options[config_name])
     bootstrap.init_app(app)
+    db.init_app(app)
     app.register_blueprint(main_blueprint)
     return app
