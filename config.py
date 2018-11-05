9df0bd329dbca3faf6279cca41acf51c1fb8ea62
Anillab <anillabusolo@gmail.com>
2018-11-06 01:14:27 +0300

    defined the manger shell decorater,imported the db

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 class Config():
     """General configuration class"""
     SECRET_KEY=os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://anilla:Busolo@1997@localhost/blog'
 class ProdConfig(Config):
     pass
 
