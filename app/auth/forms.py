36566dec8ab374b5d06660ed45611c7f2ff904ec
Anillab <anillabusolo@gmail.com>
2018-11-09 22:25:29 +0300

    created registration and login imported validators

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,20 +1,16 @@
-# from flask_wtf import FlaskForm
-# from wtforms import StringField, PasswordField , SubmitField,BooleanField
-# from wtforms.validators import DataRequired , Length , Email , EqualTo
-#
-# class RegistrationForm(FlaskForm):
-#     username = StringField('Username',validators=[DataRequired(),Length(min=2,max=20)])
-#
-#     email = StringField('Email',validators=[DataRequired(),Email()])
-#
-#     password = PasswordField('Password', validators=[DataRequired()])
-#
-#     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(),EqualTo('password')])
-#
-#     submit = SubmitField('Register')
-#
-# class LoginForm(FlaskForm):
-#     username = StringField('Username',validators=[DataRequired(),Length(min=2,max=20)])
-#     password = PasswordField('Password', validators=[DataRequired()])
-#     remember = BooleanField('remember me')
-#     submit = SubmitField('Login')
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField,SubmitField,PasswordField,BooleanField
+from wtforms.validators import DataRequired,Length,Email,EqualTo
+from ..models import User
+class RegistrationForm(FlaskForm):
+    username=StringField('Username',validators=[DataRequired()])
+    email=StringField('Email Address',validators=[DataRequired(),Email()])
+    password=PasswordField('New Password',validators=[DataRequired(),EqualTo('Confirm',message='Passwords must match')])
+    confirm=PasswordField('Repeat Password')
+    submit=SubmitField('submit')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username',validators=[DataRequired(),Length(min=2,max=20)])
+    email = StringField('Email',validators=[DataRequired(),Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
