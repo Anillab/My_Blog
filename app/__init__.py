64f8a3f80c9cf034ddd1bfdff034a2aeb637bcdb
Anillab <anillabusolo@gmail.com>
2018-11-09 22:23:56 +0300

    imported and initialized LoginManger imported and registered auth as auth_blueprinnt

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,10 @@ from flask  import *
 from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
-# from main  import hello
-# from app import errors
+from flask_login import LoginManager
+login_manager=LoginManager()
+login_manager.session_protection='strong'
+login_manager.login_view='auth.login'
 bootstrap=Bootstrap()
 db=SQLAlchemy()
 from .main import main as main_blueprint
@@ -13,5 +15,8 @@ def create_app(config_name):
     app.config.from_object(config_options[config_name])
     bootstrap.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
     app.register_blueprint(main_blueprint)
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix='/authenticate')
     return app
