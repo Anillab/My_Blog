6624f178b28f0b759cd388e03ba30d89d5fe4d57
Anillab <anillabusolo@gmail.com>
2019-03-16 09:11:43 +0300

    created the post and comment form

--- app/main/forms.py
+++ app/main/forms.py
@@ -4,3 +4,14 @@ from wtforms.validators import DataRequired,Length,Email,EqualTo
 class UpdateProfile(FlaskForm):
     bio=TextAreaField('Tell us about yourself',validators=[DataRequired()])
     submit=SubmitField('Submit')
+
+
+
+class PostForm(FlaskForm):
+    title=TextAreaField('Title',validators=[DataRequired()])
+    content=TextAreaField('Content',validators=[DataRequired()])
+    sumit=SubmitField('Post',validators=[DataRequired()])
+
+class CommentForm(FlaskForm):
+    comment=TextAreaField('Comment',validators=[DataRequired()])
+    submit=SubmitField('SubmitField',validators=[DataRequired()])
